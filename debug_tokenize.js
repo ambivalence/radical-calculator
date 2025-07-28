b3712fe4f9448ff60d1500a0b302fb3553b16a00
Yu-Shan Fung <yushan@squareup.com>
2025-07-28 02:13:24 -0700

    chore: Clean up temporary files and add calculator icon
    - Removed debug and temporary files
    - Added calculator SVG icon
    - Fixed TypeScript esModuleInterop warning

--- debug_tokenize.js
+++
@@ -1,6 +0,0 @@
-import { ExpressionParser } from './src/services/ExpressionParser.js'
-
-const parser = new ExpressionParser();
-const tokens = parser.tokenize('2 * (x + 3) - sqrt(y)');
-console.log('Tokens:', tokens.length);
-tokens.forEach((t, i) => console.log(`${i}: ${t.type} - ${t.value}`));
